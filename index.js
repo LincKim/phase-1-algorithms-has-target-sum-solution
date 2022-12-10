cc93bc888700b1e5c5459444411afc221a2e30cd
LincKim <lincolnkimutai@gmail.com>
2022-12-10 07:33:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = {}
+  for (const number of array) {
+    const otherNum = target - number
+    if (otherNum in seenNumbers) return true
+    seenNumbers[number] = true
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  Runtime: O(n)
+  Space: O(n)
 */
 
 /* 
